e459d0241effa06ea04a85305a149b0efaea18b2
Alina <aleksandrovaalina4@gmail.com>
2024-03-27 19:23:09 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const addFunction = function(a, b) {
+    return a + b;
+}
+
+const subtFunction = function(a, b) {
+    return a - b;
+}
+
+const multFunction = function(a, b) {
+    return a * b;
+}
+
+console.log(addFunction(2, 2));
+console.log(subtFunction(2, 2));
+console.log(multFunction(2, 2));
